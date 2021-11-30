caadb6032be93ad3b66b8409e85c7155f3075bd2
idantaddei <idantaddei@gmail.com>
2021-11-30 16:37:44 +0200

    add dummy data

---
+++ client/src/components/courseList.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function courseList() {
+  const courses = [
+    { id: 0, name: 'my course #0', owner: 'david' },
+    { id: 1, name: 'my course #1', owner: 'david' },
+    { id: 2, name: 'my course #2', owner: 'david' },
+    { id: 3, name: 'my course #3', owner: 'david' },
+    { id: 4, name: 'my course #4', owner: 'david' },
+    { id: 5, name: 'my course #5', owner: 'david' },
+    { id: 6, name: 'my course #6', owner: 'david' },
+    { id: 7, name: 'my course #7', owner: 'david' },
+  ];
+
+  return <div className="Course-list">adsasd</div>;
+}
+
+export default courseList;
