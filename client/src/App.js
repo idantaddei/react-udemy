cd53f628c086d58eb88420d367a9c3d4111830d7
idantaddei <idantaddei@gmail.com>
2021-11-30 17:31:24 +0200

    show course page when a course is selected

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,25 @@
 import './App.css';
 import './styles/course.css';
 
+import { useState } from 'react';
 import CourseList from './components/CourseList';
-
+import CoursePage from './components/CoursePage';
 const App = () => {
+  const [selectedCourse, setSelectedCourse] = useState(undefined);
+
   return (
     <div className="App">
       <header className="App-header">
         <p>Udemy Clone</p>
       </header>
-      <CourseList />
+      {selectedCourse ? (
+        <CoursePage
+          course={selectedCourse}
+          exitPage={() => setSelectedCourse(undefined)}
+        />
+      ) : (
+        <CourseList renderPage={setSelectedCourse} />
+      )}
       <div className="App-body"></div>
     </div>
   );
