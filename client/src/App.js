980ffeff55ed895dd7c073d5f8d9e1c195fde1d4
DolevOzVayyar <dolev.oz@vayyar.com>
2021-11-30 19:54:00 +0200

    added login basis

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,22 @@ import './styles/course.css';
 
 import CourseList from './components/CourseList';
 
+import {useState} from "react";
+
 const App = () => {
+    const [isAuth, setIsAuth] = useState(false);
   return (
     <div className="App">
       <header className="App-header">
-        <p>Udemy Clone</p>
+          <div className="App-header-item"/>
+          <div className="App-header-item">
+            <p>Udemy Clone</p>
+          </div>
+          <div className="App-header-item">
+            <button onClick={() => {setIsAuth(!isAuth)}}>sign in</button>
+          </div>
       </header>
-      <CourseList />
+        {isAuth && <CourseList />}
       <div className="App-body"></div>
     </div>
   );
